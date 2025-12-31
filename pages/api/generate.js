6ecebaa2c57bbac1c2bd39165567125f6b2d32c5
MARIANO <dixbobdemex@gmail.com>
2025-12-31 12:54:18 -0600

    Update API URL to use stable version
    se cambio v1beta por v1 en url

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
 
   // 3. Intentar conexión con URL estable
   //const url = `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${apiKey}`;
-  const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${apiKey}`;
+  const url = `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${apiKey}`;
   try {
     const response = await fetch(url, {
       method: "POST",
